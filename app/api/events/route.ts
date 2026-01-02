2716fce7a09a6072ff2b55e854acd43e81ad91ca
Tiago Lima <tiagolima@osf.digital>
2026-01-02 14:52:47 -0300

    feat: uploaded image to cloudinary. updated event image with cloudinary url.

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextRequest, NextResponse } from "next/server";
 import { Event } from "@/database";
 import connectDB from "@/lib/mongodb";
-import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
 export async function POST(req: NextRequest) {
   try {
@@ -12,9 +14,27 @@ export async function POST(req: NextRequest) {
 
     try {
       event = Object.fromEntries(formData.entries());
-    } catch (e) {
+    } catch (e: any) {
       return NextResponse.json({ message: "Invalid JSON data format." }, { status: 400 });
     }
+
+    const file = formData.get("image") as File;
+
+    if (!file) return NextResponse.json({ message: "Image is required" }, { status: 400 });
+
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+
+    const uploadResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({ resource_type: "image", folder: "events" }, (error, result) => {
+          if (error) reject(error);
+          resolve(result);
+        })
+        .end(buffer);
+    });
+
+    event.image = (uploadResult as { secure_url: string }).secure_url;
 
     const createEvent = await Event.create(event);
 
@@ -27,5 +47,17 @@ export async function POST(req: NextRequest) {
       { message: "Event creation failed", error: e instanceof Error ? e.message : "Unknown" },
       { status: 500 }
     );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectDB();
+
+    const events = await Event.find().sort({ createdAt: -1 });
+
+    return NextResponse.json({ message: "Events fetched successfully", events }, { status: 200 });
+  } catch (e: any) {
+    return NextResponse.json({ message: "Event fetching failed", error: e }, { status: 500 });
   }
 }
